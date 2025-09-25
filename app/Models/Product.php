77b3f65fd076e9ce28e7aabaa592f49bc5b2459b
Zohair Laabane Tlemcani <zohair.laabane@outlook.com>
2025-09-25 16:21:56 +0100

    feat: add product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,15 +5,25 @@ namespace App\Models;
 use Database\Factories\ProductFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use MongoDB\Laravel\Eloquent\Model;
+use MongoDB\Laravel\Relations\BelongsTo;
 
 class Product extends Model
 {
     /** @use HasFactory<ProductFactory> */
     use HasFactory, HasUuids;
 
-    protected $guarded = [];
+    protected $fillable = [
+        'name',
+        'description',
+        'sku',
+        'category_id',
+        'price',
+        'status',
+        'stock_quantity',
+        'images',
+        'variants',
+    ];
 
     public function category(): BelongsTo
     {
