058f288fc53b0e0c1802cf51c6c935bad4587313
Zohair Laabane Tlemcani <zohair.laabane@outlook.com>
2025-09-25 16:25:07 +0100

    feat: add product category relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,11 +6,17 @@ use Database\Factories\CategoryFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use MongoDB\Laravel\Eloquent\Model;
+use MongoDB\Laravel\Relations\HasMany;
 
 class Category extends Model
 {
     /** @use HasFactory<CategoryFactory> */
     use HasFactory, HasUuids;
 
-    protected $guarded = ['id'];
+    protected $fillable = ['name'];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
