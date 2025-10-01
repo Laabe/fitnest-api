37b3fdfd57f590e7af7cc58758038f7ee1fec724
Zohair Laabane Tlemcani <zohair.laabane@outlook.com>
2025-10-01 12:44:33 +0100

    refactor: user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateMyProfileRequest;
+use App\Http\Requests\UpdateUserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -28,7 +31,22 @@ class UserController extends Controller
         $user = User::create(
             array_merge(
                 $validated,
-                ['password' => Hash::make('password')] // Default password, should be changed by user later
+                ['password' => Hash::make('password')]
+            )
+        );
+        return new UserResource($user);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function update(UpdateUserRequest $request): UserResource
+    {
+        $validated = $request->validated();
+        $user = User::create(
+            array_merge(
+                $validated,
+                ['password' => Hash::make('password')]
             )
         );
         return new UserResource($user);
@@ -39,6 +57,27 @@ class UserController extends Controller
      */
     public function show(User $user): UserResource
     {
+        return new UserResource($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user): Response
+    {
+        $user->delete();
+        return response()->noContent();
+    }
+
+    public function updateMyProfile(UpdateMyProfileRequest $request): UserResource
+    {
+        /**@var User $user */
+        $user = auth()->user();
+        $validated = $request->validated();
+        if (isset($validated['password'])) {
+            $validated['password'] = Hash::make($validated['password']);
+        }
+        $user->update($validated);
         return new UserResource($user);
     }
 }
