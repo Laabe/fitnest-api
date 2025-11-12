4b9e6f4f4dcbbc2c59e531d0904bd5443d3d5371
Zohair Laabane Tlemcani <zohair.laabane@outlook.com>
2025-11-12 13:16:00 +0100

    feat: refactor authentication routes and improve response structure

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use MongoDB\Laravel\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
